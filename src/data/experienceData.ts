784f80409785da27667a9ccd955dec1ce0fd32e4
guillermo <gui.duquesanchez@gmail.com>
2025-01-02 16:25:14 -0300

    first commit

---
+++ src/data/experienceData.ts
@@ -0,0 +1,15 @@
+// src/data/experienceData.ts
+export const EXPERIENCE = [
+    {
+        date: 'January 2022 - Present',
+        title: 'Senior Software Developer',
+        description: 'Lead the development of backend microservices for complex systems, leveraging technologies such as Spring Boot, Spring Cloud, and relational/non-relational databases (PostgreSQL, MongoDB). Designed and optimized CI/CD pipelines using tools like Jenkins and Docker, reducing deployment times by 30%. Implemented authentication and authorization solutions based on OAuth 2.0 and JWT, enhancing security and user experience. Played a key role in migrating legacy systems to microservices architecture.',
+        link: 'https://www.ginkofs.com/',
+    },
+    {
+        date: 'October 2017 - January 2022',
+        title: 'Software Developer',
+        description: 'Developed and integrated RESTful APIs for enterprise applications using Spring Boot and Kotlin, delivering modern and concise solutions for microservices. Worked on high-concurrency projects, implementing caching strategies and optimizing SQL queries for better performance. Automated testing and ensured deliverable quality with tools like JUnit, Mockito, and Kotlin\'s test frameworks. Collaborated with cross-functional teams to design scalable, Kotlin-powered microservices serving thousands of simultaneous users.',
+        link: 'https://activeit.com/',
+    },
+];
